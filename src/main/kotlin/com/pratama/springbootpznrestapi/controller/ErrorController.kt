45f1d2bcbc1b6fc29259b370cf71feda83cea498
pratamawijaya <set.mnemonix@gmail.com>
2022-06-03 20:26:20 +0700

    11. improve error message# If applied, this commit will...

--- src/main/kotlin/com/pratama/springbootpznrestapi/controller/ErrorController.kt
+++ src/main/kotlin/com/pratama/springbootpznrestapi/controller/ErrorController.kt
@@ -1,6 +1,8 @@
 package com.pratama.springbootpznrestapi.controller
 
 import com.pratama.springbootpznrestapi.model.WebResponse
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -10,13 +12,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 @RestControllerAdvice
 class ErrorController {
 
+    val logger: Logger = LoggerFactory.getLogger(ErrorController::class.java)
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException::class)
     fun validationHandler(ex: MethodArgumentNotValidException): WebResponse<String> {
+        logger.info(ex.toString())
+        val data = ex.bindingResult.fieldErrors
+        logger.info("data binding result : ${data.size} : $data")
         return WebResponse(
             code = 400,
             status = "Bad Request",
-            data = ex.localizedMessage
+            data = "${data[0].field} ${data[0].defaultMessage}"
         )
     }
 }
