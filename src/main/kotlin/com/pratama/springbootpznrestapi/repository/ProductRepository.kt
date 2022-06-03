590625c931a9d350840127e427c4e8543f279a7e
pratamawijaya <set.mnemonix@gmail.com>
2022-06-03 17:45:08 +0700

    3. create product repository

---
+++ src/main/kotlin/com/pratama/springbootpznrestapi/repository/ProductRepository.kt
@@ -0,0 +1,8 @@
+package com.pratama.springbootpznrestapi.repository
+
+import com.pratama.springbootpznrestapi.entity.Product
+import org.springframework.data.jpa.repository.JpaRepository
+
+
+interface ProductRepository:JpaRepository<Product,String> {
+}
