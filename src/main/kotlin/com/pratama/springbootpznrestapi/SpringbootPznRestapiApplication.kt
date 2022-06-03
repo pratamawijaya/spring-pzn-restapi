c39bc3d07fa0847c681d7b01f8c7c9cad9ce93ca
pratamawijaya <set.mnemonix@gmail.com>
2022-06-03 17:33:24 +0700

    init project

---
+++ src/main/kotlin/com/pratama/springbootpznrestapi/SpringbootPznRestapiApplication.kt
@@ -0,0 +1,11 @@
+package com.pratama.springbootpznrestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootPznRestapiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootPznRestapiApplication>(*args)
+}
---
+++ src/test/kotlin/com/pratama/springbootpznrestapi/SpringbootPznRestapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pratama.springbootpznrestapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootPznRestapiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
