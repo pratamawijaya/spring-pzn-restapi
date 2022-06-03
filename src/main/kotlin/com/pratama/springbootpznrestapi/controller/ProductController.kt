9a358ab11b9314ea06fdd600d74adb559a1b4341
pratamawijaya <set.mnemonix@gmail.com>
2022-06-03 17:56:03 +0700

    5. create products controller

---
+++ src/main/kotlin/com/pratama/springbootpznrestapi/controller/ProductController.kt
@@ -0,0 +1,29 @@
+package com.pratama.springbootpznrestapi.controller
+
+import com.pratama.springbootpznrestapi.model.ProductResponse
+import com.pratama.springbootpznrestapi.model.WebResponse
+import com.pratama.springbootpznrestapi.model.request.CreateProductRequest
+import com.pratama.springbootpznrestapi.service.ProductService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ProductController(val service: ProductService) {
+
+    @PostMapping(
+        value = ["/api/products"],
+        produces = ["applicatio/json"],
+        consumes = ["application/json"]
+    )
+    fun createProduct(@RequestBody body: CreateProductRequest): WebResponse<ProductResponse> {
+        val response = service.create(body)
+        // TODO: 03/06/22 error handling for another http response code
+        return WebResponse(
+            code = 200,
+            status = "OK",
+            data = response
+        )
+    }
+}
---
+++ src/main/kotlin/com/pratama/springbootpznrestapi/model/WebResponse.kt
@@ -0,0 +1,7 @@
+package com.pratama.springbootpznrestapi.model
+
+data class WebResponse<T>(
+    val code: Int,
+    val status: String,
+    val data: T
+)
